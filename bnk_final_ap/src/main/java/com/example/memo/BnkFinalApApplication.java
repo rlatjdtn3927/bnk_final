3e6d811b8c45989613211d461e669b80c50f20c5
hpkim <rlatjdtn3927@gmail.com>
2025-07-30 10:37:39 +0900

    publish
    first publish

---
+++ bnk_final_ap/src/main/java/com/example/memo/BnkFinalApApplication.java
@@ -0,0 +1,13 @@
+package com.example.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnkFinalApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnkFinalApApplication.class, args);
+	}
+
+}
