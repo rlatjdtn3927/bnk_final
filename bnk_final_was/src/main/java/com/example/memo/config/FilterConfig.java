cfb64d0c75ba8fc69fd057c734b78dcf98b9b932
sujiinnn <sujiinnn@gmail.com>
2025-08-18 20:35:06 +0900

    로그인 필터 주석

--- bnk_final_was/src/main/java/com/example/memo/config/FilterConfig.java
+++ bnk_final_was/src/main/java/com/example/memo/config/FilterConfig.java
@@ -9,7 +9,7 @@ import com.example.memo.filter.LoginCheckFilter;
 @Configuration
 public class FilterConfig {
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean<LoginCheckFilter> loginCheckFilter() {
         FilterRegistrationBean<LoginCheckFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new LoginCheckFilter());
