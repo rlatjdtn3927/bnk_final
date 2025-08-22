c3cca1247b829cf69a79c041660da37d719052d5
Jeong-Jae-Yoon <jst5769@gmail.com>
2025-08-22 20:03:04 +0900

    id 타입 일치

--- bnk_final_ap/src/main/java/com/example/memo/purchase/change/dto/request/SoldPrincipalDto.java
+++ bnk_final_ap/src/main/java/com/example/memo/purchase/change/dto/request/SoldPrincipalDto.java
@@ -15,6 +15,6 @@ import lombok.Setter;
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SoldPrincipalDto {
-	private String prodId;
+	private Long id; //원리금 보장상품의 PK가 아니라 예금보유상품 principalLedger.id ->pk를 was에서 받아 ap로 보내자
 	private Integer ratio;
 }
